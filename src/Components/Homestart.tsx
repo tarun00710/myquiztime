05e1b75dee61bc332dd604ef925fd2c849ddba1e
tarun00710 <tarunlal07@gmail.com>
2022-03-11 23:23:31 +0530

    update-style

--- src/Components/Homestart.tsx
+++ src/Components/Homestart.tsx
@@ -5,7 +5,7 @@ import { userSignInContext } from '../Context/SignInContext'
 const Homestart = () => {
     const {token} = useContext(userSignInContext)
   return (
-      <div className='bg-quizNight h-screen bg-cover bg-no-repeat bg-center opacity-80 flex justify-center align-items'>
+      <div className='bg-quizNight h-screen bg-cover bg-no-repeat bg-center opacity-80 flex justify-center align-items -mt-10'>
       <div className='mt-16'>
          <NavLink to={token ? '/Home' : '/login'}><button className='bg-orange p-3 rounded text-lg mt-16 text-white'>Lets Go ➡</button></NavLink>
       </div>
